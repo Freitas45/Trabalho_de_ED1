caca6c5ea63cc3288d90ad5965203317fe29ef4a
Freitas45 <ggddfreitas@gmail.com>
2021-08-09 16:47:27 -0300

    Initial commit

---
+++ Pessoas.c
@@ -0,0 +1,5 @@
+//
+// Created by ggddf on 09/08/2021.
+//
+
+#include "Pessoas.h"
---
+++ Pessoas.h
@@ -0,0 +1,8 @@
+//
+// Created by ggddf on 09/08/2021.
+//
+
+#ifndef TRABALHO_DE_ED1_PESSOAS_H
+#define TRABALHO_DE_ED1_PESSOAS_H
+
+#endif //TRABALHO_DE_ED1_PESSOAS_H
